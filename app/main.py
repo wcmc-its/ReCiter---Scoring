ea23ab1b39859ca6e4e013dbd4f1eb2b251e3507
mrj4001 <mrj4001@med.cornell.edu>
2025-07-10 15:17:04 +0530

    debugging log

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import subprocess
 import logging
 import os,sys
 # Set up logging configuration
-logging.basicConfig(filename='ReCiterScoring.log', level=logging.INFO, 
+logging.basicConfig(level=logging.INFO, 
                     format='%(asctime)s - %(levelname)s - %(message)s',
                     handlers=[logging.StreamHandler(sys.stderr)])
 
